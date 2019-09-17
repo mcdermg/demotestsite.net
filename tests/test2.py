79199c824df5de7c83472b0175b534c97118ab0e
Gary Mc Dermott <mcdermg@tcd.ie>
2019-09-16 22:48:38 -0300

    added python exit code

--- tests/test2.py
+++ tests/test2.py
@@ -14,8 +14,11 @@ browser.get('https://www.demotestsite.net')
 if(browser.title=="Gary McDermott"):
     browser.save_screenshot('/home/tests/picture-'+timestr+'.png')
     print ("Success: Main blog page exists")
+    print exit(0)
 else:
     print ("Test failed: page title is incorrect")
+    browser.save_screenshot('/home/tests/picture-'+timestr+'.png')
     # python Datadog raise alert
+    print exit(1)
 
 browser.quit()
