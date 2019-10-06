e55f39d80a1b7bb62c8a512bbbc43e27f7a8ab8d
Gary Mc Dermott <mcdermg@tcd.ie>
2019-10-06 20:47:26 -0300

    messed up git using rm cache for some stupid reason. re adding everything

--- tests/sitetest1.py
+++ tests/sitetest1.py
@@ -1,26 +1,42 @@
+# Import depdency libraries
 from selenium import webdriver
 import time
+import logging
 
+# For logging all events info and above and formatting with log level, datetime and message
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Timstring for datetime format used in screenshot output
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
+# Location of Chromedriver Linux used in pipline. Windows forr local testing.
 chromedriver = '/usr/bin/chromedriver'
 #chromedriver = 'C:/repos/demotestsite.net/chromedriver/chromedriver.exe'
 
+# Set options for Selienium & Chromedriver. Run headless etc
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+
+# url to be retrieved
 browser.get('https://www.demotestsite.net')
 
+# Test
 if(browser.title=="Gary Mc Dermott"):
     browser.save_screenshot('./screenshots/test1-pass-'+timestr+'.png')
-    print ("Test passed: Page title is correct")
-    print exit(0)
+    #print ("Test passed: Page title is correct")
+    logging.info("Test passed: Page title is correct")
+    logging.info(exit(0))
+    #print exit(0)
 else:
-    print ("Test failed: Page title is incorrect")
     browser.save_screenshot('./screenshots/test1-fail-'+timestr+'.png')
-    # python Datadog raise alert
-    print exit(1)
+    # TODO python Datadog raise alert
+    logging.error("Test failed: Page title is incorrect")
+    logging.error(exit(1))
+    #print ("Test failed: Page title is incorrect")
+    #print exit(1)
 
+# Close off webdriver & selienium
 browser.quit()
--- tests/sitetest2.py
+++ tests/sitetest2.py
@@ -1,37 +1,55 @@
+# Import depdency libraries
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
+import logging
 
+# For logging all events info and above and formatting with log level, datetime and message
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Timstring for datetime format used in screenshot output
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
+# Location of Chromedriver Linux used in pipline. Windows forr local testing.
 chromedriver = '/usr/bin/chromedriver'
 #chromedriver = 'C:/repos/demotestsite.net/chromedriver/chromedriver.exe'
 
+# Set options for Selienium & Chromedriver. Run headless etc
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
 
+# url to be retrieved
 browser.get('https://www.demotestsite.net')
 
+# test. using try catch as if element does not exist exception is thrown.
+# Exception does not get thrown anymore with else in place. keeping as legacy as occured in passed.
 try:
     #x=browser.find_elements_by_id("navbarCollapse")
     #x=browser.find_elements_by_id("naddaaaaaaa")
     #x=browser.find_elements_by_xpath("//a[contains(text(),'naddaaaaaaa')]")
     x=browser.find_elements_by_xpath("//a[contains(text(),'About')]")
     if(len(x)>0):
-        print ("Success: Found About in navbar")
         browser.save_screenshot('./screenshots/test2-pass-'+timestr+'.png')
-        print exit(0)
+        logging.info("Success: Found About in navbar")
+        logging.info(exit(0))
+        #print ("Success: Found About in navbar")
+        #print exit(0)
 except NoSuchElementException:
-    print("No element found. Breaking...")
     browser.save_screenshot('./screenshots/test2-NoSuchElementException-'+timestr+'.png')
-    print exit(1)
+    logging.critical("No such element found exception.")
+    logging.critical(exit(1))
+    #print("No element found. Breaking...")
+    #print exit(1)
 else:
     if(len(x)==0):
-        print ("Test failed: Could not find About in navbar")
         browser.save_screenshot('./screenshots/test2-fail-'+timestr+'.png')
-        print exit(1)
+        logging.error("Test failed: Could not find About in navbar")
+        logging.error(exit(1))
+        #print ("Test failed: Could not find About in navbar")
+        #print exit(1)
 
+# Close off webdriver & selienium
 browser.quit()
